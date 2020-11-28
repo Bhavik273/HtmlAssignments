100811e77e092be1ba407b04d989263d0bfcd621
Bhavik273 <mbhavik273@gmail.com>
2020-11-28 15:44:24 +0530

    Source Conrol Assignment1

---
+++ SourceControlAssignment1/ValidatorsDemo/ValidatorsDemo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ValidatorsDemo.Models;
+
+namespace ValidatorsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return RedirectToAction("Create");
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["student"] = student;
+                return RedirectToAction("Display");
+            }
+
+            return View();
+        }
+        public ActionResult Display()
+        {
+            Student student = TempData["student"] as Student;
+            return View(student);
+        }
+    }
+}
---
+++ SourceControlAssignment1/ValidatorsDemo/ValidatorsDemo/Models/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ValidatorsDemo.Custom_Validators;
+
+namespace ValidatorsDemo.Models
+{
+    public class Student
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter position")]
+        [DepartmentValidator]
+        public string Department { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter Birth date")]
+        [Display(Name = "Birth Date")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        [BirthDate(ErrorMessage = "Birthdate Date must be less than or equal to Today's Date")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Enter Semester")]
+        [Range(1,8,ErrorMessage ="Value must be between 1 and 8")]
+        public int Semester { get; set; }
+
+        [Required(ErrorMessage ="Enter Gender")]
+        [RegularExpression("^Male|Female$",ErrorMessage ="Must be Male or Female")]
+        public string Gender { get; set; }
+    }
+
+    public class DepartmentValidator : ValidationAttribute
+    {
+        private string[] Departments = { "CE", "EC", "IT" };
+        public override bool IsValid(object value)
+        {
+            return Departments.Contains(value.ToString());
+        }
+    }
+}
